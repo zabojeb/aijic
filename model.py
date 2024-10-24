c4300009bc7998aef0be3de2d022486ddb66841d
Robot-mitski <118906932+Robot-mitski@users.noreply.github.com>
2024-10-24 23:18:05 +0300

    Update model.py

--- model.py
+++ model.py
@@ -73,3 +73,20 @@ def ton_analyse(text):
 
 def tests_analyse(df):
     return reviews(df)
+
+model_checkpoint = 'cointegrated/rubert-base-cased-nli-threeway'
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint)
+if torch.cuda.is_available():
+    model.cuda()
+
+def predict_zero_shot(text, label_texts, model, tokenizer, label='entailment', normalize=True):
+    label_texts
+    tokens = tokenizer([text] * len(label_texts), label_texts, truncation=True, return_tensors='pt', padding=True)
+    with torch.inference_mode():
+        result = torch.softmax(model(**tokens.to(model.device)).logits, -1)
+    proba = result[:, model.config.label2id[label]].cpu().numpy()
+    if normalize:
+        proba /= sum(proba)
+    return proba
+aspects1 = ['практика','теория','преподаватель','технологии','актуальность']
